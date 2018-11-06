d2044095310368227b66b07c9ec431b4eead7f0e
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 08:03:51 -0300

    Tenth Performance/Program made in C#

---
+++ TenthPerformance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TenthPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a, b;
+            n = int.Parse(Console.ReadLine());
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            for (int i=a; i<b; i++)
+            {
+                if(i % n == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
